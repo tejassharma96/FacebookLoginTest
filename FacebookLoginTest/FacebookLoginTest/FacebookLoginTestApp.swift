76aa515bdfeb62fecad011fca0e575d4cd572dfa
Tejas Sharma <tejass@zillowgroup.com>
2024-09-30 14:54:07 -0700

    create sample repo

---
+++ FacebookLoginTest/FacebookLoginTest/FacebookLoginTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  FacebookLoginTestApp.swift
+//  FacebookLoginTest
+//
+//  Created by Tejas Sharma on 9/30/24.
+//
+
+import SwiftUI
+import FacebookLogin
+
+@main
+struct FacebookLoginTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
